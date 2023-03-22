a3ef8785a7153ecd8c3185aefd330665e2412c63
EC2 Default User <126657775+shizuki-okuma@users.noreply.github.com>
2023-03-22 14:31:09 +0000

    [app2]rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
   get "home/about"=>"homes#about"
+  root to: 'homes#top'
   
   resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :update, :destroy]
