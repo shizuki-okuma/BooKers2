a05218113e505f29aaeff0a829ecae1121625311
EC2 Default User <126657775+shizuki-okuma@users.noreply.github.com>
2023-04-05 05:09:03 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-  
   end
   
   def edit
     @user = User.find(params[:id])
     @user = current_user
   end
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "home/about"=>"homes#about"
+  get "homes/about"=>"homes#about"
   root to: 'homes#top'
-  
   resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
